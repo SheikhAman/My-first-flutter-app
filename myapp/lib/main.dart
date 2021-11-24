b067d08b967053b539c7c24da3a362ce6630abc5
ShekAdnan <adnansk418@gmail.com>
2021-11-24 16:49:14 +0600

    _buildSuggestion(),_buildRow

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -11,17 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Welcome to flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Welcome to flutter',
-          ),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -34,9 +26,44 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerfont = TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordpair = WordPair.random();
-    return Text(wordpair.asPascalCase);
+    // final wordpair = WordPair.random(); // delete this
+    // return Text(wordpair.asPascalCase); // two lines
+    return Scaffold(
+      // start from here
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            // i bigor hole divider dibe
+            return Divider();
+          }
+          final int index = i ~/ 2; //  2 diye  vag kore vagfol integer nibe
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerfont,
+      ),
+    );
   }
 }
